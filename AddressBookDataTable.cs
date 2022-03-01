02baf20b8e7fa28ec79562d00c00941613ca9f8b
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-03-01 23:23:13 +0530

    Create Address Book Data Base Table

---
+++ AddressBookDataTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem_Linq
+{
+    class AddressBookDataTable
+    {
+        public readonly DataTable dataTable = new DataTable();
+        public DataTable CreateTable(AddrssBookModel model)
+        {
+            var taleColumn1 = new DataColumn("First_Name");
+            dataTable.Columns.Add(taleColumn1);
+            var taleColumn2 = new DataColumn("Last_Name");
+            dataTable.Columns.Add(taleColumn2);
+            var taleColumn3 = new DataColumn("Address");
+            dataTable.Columns.Add(taleColumn3);
+            var taleColumn4 = new DataColumn("City");
+            dataTable.Columns.Add(taleColumn4);
+            var taleColumn5 = new DataColumn("State");
+            dataTable.Columns.Add(taleColumn5);
+            var taleColumn6 = new DataColumn("Zip");
+            dataTable.Columns.Add(taleColumn6);
+            var taleColumn7 = new DataColumn("Phone_Number");
+            dataTable.Columns.Add(taleColumn7);
+            var taleColumn8 = new DataColumn("Email");
+            dataTable.Columns.Add(taleColumn8);
+            return dataTable;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using AddressBookSystem_Linq;
+
+Console.WriteLine("Welcome_To_AddressBookSystem_Linq");
+AddrssBookModel model = new AddrssBookModel();
+AddressBookDataTable dataTable = new AddressBookDataTable();
+dataTable.CreateTable(model);
